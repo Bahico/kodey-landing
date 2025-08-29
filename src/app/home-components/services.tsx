631b41f0fe85e15852fc984b8c000d0061deb33c
uniconsoft-macmini-1 <ikromov0312@gmail.com>
2025-08-29 20:39:04 +0500

    asf

--- src/app/home-components/services.tsx
+++ src/app/home-components/services.tsx
@@ -41,8 +41,8 @@ export default function Services() {
 
     return (
         <section id="services" className="bg-black flex justify-center w-full pt-20 sm:pt-30 md:pt-40">
-            <div className="container px-4 sm:px-6 lg:px-8">
-                <div className="mb-12 sm:mb-16 lg:mb-20">
+            <div className="w-full flex flex-col items-center">
+                <div className="container mb-12 sm:mb-16 lg:mb-20">
                     <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6 lg:mb-8 text-white">
                         Digital <span className="text-gray-400">— инструментарий</span>
                     </h2>
@@ -51,14 +51,19 @@ export default function Services() {
                     </p>
                 </div>
 
-                <div className="flex overflow-x-auto pb-6 sm:pb-8 lg:pb-10 gap-4 sm:gap-6 md:gap-8 lg:gap-12 scrollbar-hide">
+                <div className="overflow-visible services-container flex w-full overflow-x-auto pb-6 sm:pb-8 lg:pb-10 gap-4 sm:gap-6 md:gap-8 lg:gap-12 scrollbar-hide">
                     {services.map((service, index) => (
-                        <div key={index} className={`group relative h-[400px] sm:h-[500px] md:h-[600px] lg:h-[730px] flex items-end w-[280px] sm:w-[320px] md:w-[380px] lg:w-[465px] min-w-[280px] sm:min-w-[320px] md:min-w-[380px] lg:min-w-[465px] rounded-xl sm:rounded-2xl ${service.active ? 'bg-black bg-opacity-90' : 'bg-black bg-opacity-60'}`}>
-                            <div className="absolute top-0 w-full h-[320px] sm:h-[400px] md:h-[480px] lg:h-[600px] flex justify-center items-center z-10">
-                                <img src="images/home/60b300101d6e7d385987432f11adc9d89f983db0.png" className="grayscale group-hover:grayscale-0 transition-all duration-300 w-2/3 h-full object-contain" alt="" />
+                        <div key={index}
+                             className={`group card-group relative h-[400px] sm:h-[500px] md:h-[600px] lg:h-[730px] flex items-end w-[280px] sm:w-[320px] md:w-[380px] lg:w-[465px] min-w-[280px] sm:min-w-[320px] md:min-w-[380px] lg:min-w-[465px] rounded-xl sm:rounded-2xl bg-black hover:bg-opacity-90 bg-opacity-60`}>
+                            <div
+                                className="absolute top-0 w-full h-[320px] sm:h-[400px] md:h-[480px] lg:h-[600px] flex justify-center items-center z-10">
+                                <img src="images/home/60b300101d6e7d385987432f11adc9d89f983db0.png"
+                                     className="grayscale group-hover:grayscale-0 transition-all duration-300 w-2/3 h-full object-contain"
+                                     alt=""/>
                             </div>
 
-                            <div className="flex flex-col justify-end p-4 sm:p-6 lg:p-8 h-[280px] sm:h-[320px] md:h-[380px] lg:h-[465px] rounded-xl sm:rounded-2xl flex flex-col justify-between bg-[#0C0C0C99] group-hover:bg-[#0C0C0CE5] transition-all duration-300 card-body">
+                            <div
+                                className="card-shadow p-4 sm:p-6 lg:p-8 h-[280px] sm:h-[320px] md:h-[380px] lg:h-[465px] rounded-xl sm:rounded-2xl flex flex-col justify-end bg-[#0C0C0C99] group-hover:bg-[#0C0C0CE5] transition-all duration-300 card-body">
                                 <div>
                                     <h3 className={`text-lg sm:text-xl lg:text-2xl font-semibold mb-2 sm:mb-3 lg:mb-4 text-gray-500 group-hover:text-gray-200 transition-all duration-300`}>
                                         {service.title}
