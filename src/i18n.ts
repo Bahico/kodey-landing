42f99f7a4381e0c75bb64da41700972924a5a3c1
uniconsoft-macmini-1 <ikromov0312@gmail.com>
2025-09-07 13:29:29 +0500

    change error

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,11 @@ import {getRequestConfig} from 'next-intl/server';
 export const locales = ['en', 'ru'] as const;
 export type Locale = typeof locales[number];
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async (params) => {
+  const { locale } = await params;
+  const messages = (await import(`./messages/${locale}.json`)).default;
   return {
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages,
+    locale
   };
 });
