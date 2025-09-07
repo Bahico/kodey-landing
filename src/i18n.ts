a524bca45731060e93d31eb88d5522ab700613f6
uniconsoft-macmini-1 <ikromov0312@gmail.com>
2025-09-07 13:35:51 +0500

    bug fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,11 +3,9 @@ import {getRequestConfig} from 'next-intl/server';
 export const locales = ['en', 'ru'] as const;
 export type Locale = typeof locales[number];
 
-export default getRequestConfig(async (params) => {
-  const { locale } = await params;
-  const messages = (await import(`./messages/${locale}.json`)).default;
+export default getRequestConfig(async ({locale}) => {
   return {
-    messages,
-    locale
+    messages: (await import(`./messages/${locale}.json`)).default,
+    locale,
   };
 });
